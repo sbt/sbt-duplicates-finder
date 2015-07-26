e1f7f69b1220613769bd7a4fcc66e85770503220
Pierre DAL-PRA <dalpra.pierre@gmail.com>
2015-07-26 22:32:43 +0200

    Do not display files with equal content by default

--- src/main/scala/com/typesafe/sbt/duplicates/DuplicatesFinderKeys.scala
+++ src/main/scala/com/typesafe/sbt/duplicates/DuplicatesFinderKeys.scala
@@ -6,5 +6,5 @@ object DuplicatesFinderKeys {
 
   lazy val checkDuplicates = taskKey[Unit]("Check classpath for class or resources duplicates")
   lazy val excludePatterns = settingKey[Seq[String]]("Patterns to exclude when looking for duplicates")
-
+  lazy val reportDuplicatesWithSameContent = settingKey[Boolean]("Report duplicates even file content is the same")
 }
--- src/main/scala/com/typesafe/sbt/duplicates/DuplicatesFinderPlugin.scala
+++ src/main/scala/com/typesafe/sbt/duplicates/DuplicatesFinderPlugin.scala
@@ -16,24 +16,28 @@ object DuplicatesFinderPlugin extends AutoPlugin {
 
   lazy val baseSettings = Seq(
     excludePatterns := List("^META-INF/.*"),
+    reportDuplicatesWithSameContent := false,
     checkDuplicates <<= checkDuplicates0
   )
 
   private lazy val checkDuplicates0 = Def.task {
     val log = streams.value.log
+    val reportSameContent = reportDuplicatesWithSameContent.value
     val classpath = Classpath(fullClasspath.value.files, excludePatterns.value)
-    logDuplicates(classpath.classesDuplicates, log, "classes")
-    logDuplicates(classpath.resourcesDuplicates, log, "resources")
+    logDuplicates(classpath.classesDuplicates, log, "classes", reportSameContent)
+    logDuplicates(classpath.resourcesDuplicates, log, "resources", reportSameContent)
   }
 
-  private def logDuplicates(duplicates: List[Conflict], log: Logger, name: String) =
+  private def logDuplicates(duplicates: List[Conflict], log: Logger, name: String, reportIfSameContent: Boolean) =
     if(duplicates.nonEmpty) {
       log.warn(s"Detected $name conflicts:")
       duplicates.foreach { conflict =>
-        log.warn("")
-        log.warn(s"- ${conflict.name}: ${conflict.conflictState}")
-        conflict.conflicts.foreach { file =>
-          log.warn(s"\t - $file")
+        if(!(reportIfSameContent && conflict.conflictState == ConflictState.ContentEqual)) {
+          log.warn("")
+          log.warn(s"- ${conflict.name}: ${conflict.conflictState}")
+          conflict.conflicts.foreach { file =>
+            log.warn(s"\t - $file")
+          }
         }
       }
     }
